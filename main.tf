fcec7a43d77d7b058f82232ce9f393cf46368902
Amrut2121 <36921589+Amrut2121@users.noreply.github.com>
2020-01-21 16:35:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "My_instance"{
 	ami           = "ami-09d069a04349dc3cb"
 	instance_type = "t2.micro"
-	key_name      = "MyKey"
+	key_name      = "MYKey"
 	
 	tags = {
 	Name = "MyFirstTerraIns"
