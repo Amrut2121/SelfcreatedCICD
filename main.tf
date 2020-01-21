4a9cdaccfa0915a293e9f9a40f6bff8ec05936dc
Amrut2121 <36921589+Amrut2121@users.noreply.github.com>
2020-01-21 16:34:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-resource "aws_instance" "default"{
+resource "aws_instance" "My_instance"{
 	ami           = "ami-09d069a04349dc3cb"
 	instance_type = "t2.micro"
 	key_name      = "MyKey"
